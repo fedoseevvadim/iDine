b80fc822e3fc6f3d7980f24ed482f8506eadbf63
Vadim <fedoseevvadim@gmail.com>
2019-11-03 16:44:51 +0300

    Menu has been done with row design

--- iDine/ContentView.swift
+++ iDine/ContentView.swift
@@ -9,12 +9,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            List {
+                ForEach (menu) { section in
+                    
+                    Section(header: Text(section.name)) {
+
+                        ForEach (section.items) { item in
+                            ItemRow(item: item)
+                        }
+                        
+                    }
+                    
+                    
+                }
+            }.navigationBarTitle("Menu")
+            .listStyle(GroupedListStyle())
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    
+    
+    
     static var previews: some View {
         ContentView()
     }
